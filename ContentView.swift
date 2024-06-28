ed3b306fe6d04800c8cd6fdbeb6648686548184c
Chestha Bisht <bishtchestha@gmail.com>
2024-06-28 10:33:17 -0500

    added a toggle

--- ContentView.swift
+++ ContentView.swift
@@ -1,13 +1,19 @@
 import SwiftUI
 
+// Great Job
+
 struct ContentView: View {
+    @State private var toggle: Bool = true
+    
     var body: some View {
         VStack {
-            Text("Hello world")
+            Toggle("Light Mode", isOn: $toggle)
+                .padding()
+                .offset(x: 0, y: -350)
+                .foregroundStyle(toggle ? Color.black : Color.white)
         }
-        //Testing some more
-        
-        //Done Helping
-        //Great Job
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(toggle ? Color.white : Color.black)
+        .edgesIgnoringSafeArea(.all)
     }
 }
