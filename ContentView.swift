759e321ee08cdc757c01409529559a82d2e73027
Chestha Bisht <bishtchestha@gmail.com>
2024-06-28 10:42:50 -0500

    completed the beautiful page

--- ContentView.swift
+++ ContentView.swift
@@ -7,10 +7,19 @@ struct ContentView: View {
     
     var body: some View {
         VStack {
+            
+            Text("\n\n")
+            
+            Text("Daily Motivation")
+                .font(.largeTitle)
+                .foregroundStyle(toggle ? Color.black : Color.white)
+            Spacer()
+            
             Toggle("Light Mode", isOn: $toggle)
                 .padding()
-                .offset(x: 0, y: -350)
                 .foregroundStyle(toggle ? Color.black : Color.white)
+                .frame(width:200, height: 50)
+            Text("\n")
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(toggle ? Color.white : Color.black)
